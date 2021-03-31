e18ccbc9b6b0665f3582008b4c0c5c08c0fae171
Victor Balbino <victorbalbinoaraujo1@outlook.com>
2021-03-31 16:41:37 -0300

    Autoclicker in Python

---
+++ Misc/Autoclicker.py
@@ -0,0 +1,19 @@
+# pip install pynput
+# pip install keyboard
+import time
+from pynput import keyboard
+from pynput.mouse import Button, Controller
+import keyboard
+
+mouse = Controller()
+# You can check your mouse position by creating an infinite loop printing the position of your mouse. Then, choose where you want
+# to auto click and set the coordinates.
+# while True:
+#    print(mouse.position)
+mouse.position = (1046, 383) # Change this values accordingly to your need.
+while True:
+    if keyboard.is_pressed('q'): # Hold q to stop.
+        print("Exit")
+        break
+    time.sleep(0.10) # Seconds. It may break if it is TOO fast.
+    mouse.click(Button.left, 1)
