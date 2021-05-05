36eb7ab92fefcdeafd6bd51470eca865029ad392
Victor Balbino <victorbalbinoaraujo1@outlook.com>
2021-05-05 17:47:25 -0300

    Studying Python

--- OOP/Bank Account.py
+++ OOP/Bank Account.py
@@ -1,18 +1,18 @@
 class Conta:
 
     def __init__(self, num_conta, saldo):
-        self.num_conta = num_conta
-        self.saldo = saldo
+        self.__num_conta = num_conta
+        self.__saldo = saldo
 
     def deposito(self, valor):
         if valor > 0:
-            self.saldo += valor
+            self.__saldo += valor
         else:
             print('Valor não pode ser 0')
 
     def saque(self, valor):
-        if  self.saldo >= valor:
-            self.saldo -= valor
+        if  self.__saldo >= valor:
+            self.__saldo -= valor
             print('Operação realizada com sucesso!')
         else:
             print('Saldo insuficiente!')
@@ -21,25 +21,45 @@ class Conta:
             self.saque(valor)
             destino.deposito(valor)
  
-    def mostrar_saldo(self):
-        print(f'Saldo R$ {self.saldo}')
+    def saldo(self):
+        print(f'Saldo R$ {self.__saldo}')
+
+    def mostrar_num_conta(self):
+        return f' {self.__num_conta}'
+
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @saldo.setter
+    def saldo(self, saldo):
+        self.__saldo = saldo
 
 class Cliente(Conta):
     def __init__(self, num_conta, saldo, nome, cpf, rg, cidade):
         super().__init__(num_conta, saldo)
-        self.nome = nome
-        self.cpf = cpf
-        self.rg = rg
-        self.cidade = cidade
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__rg = rg
+        self.__cidade = cidade
+    
+    def info_pessoa(self):
+        print(f'Nome: {self.__nome}\nCPF: {self.__cpf}\nRG: {self.__rg}\nCidade: {self.__cidade}\nNúmero da Conta{self.mostrar_num_conta()}\n')
+
+    @property
+    def nome(self):
+        return self.__nome
 
 
-conta1 = Conta(num_conta=1, saldo=150)
-conta2 = Conta(num_conta=2, saldo=90)
+cliente1 = Cliente(num_conta=1, saldo=100, nome="Joe", cpf="123.456.789.91", rg="12.345.678-9", cidade="Cidade Qualquer")
+cliente2 = Cliente(num_conta=2, saldo=100, nome="Mary", cpf="321.654.987.19", rg="21.543.876-0", cidade="Outra Cidade Qualquer")
+cliente3 = Cliente(num_conta=3, saldo=200, nome="John", cpf="221.554.887.99", rg="11.443.776-1", cidade="Outra Cidade Qualquer")
+clientes = [cliente1, cliente2, cliente3]
 
-conta1.transferencia(conta2, 30)
-conta2.mostrar_saldo()
+def mostra_clientes(clientes):
+    for cliente in clientes:
+        print(cliente.info_pessoa())
 
-
-
-
-
+nome = str(input("Digite o nome: "))
+cliente4 = Cliente(num_conta=4, saldo=100, nome=nome, cpf="221.554.887.99", rg="11.443.776-1", cidade="Outra Cidade Qualquer")
+print(cliente4.nome)
