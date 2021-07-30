72492582f7f52d67ae9422b0fe64481625a35623
Victor Balbino <victorbalbinoaraujo1@outlook.com>
2021-07-30 20:31:16 -0300

    Add new function to Linked List

--- Estrutura de Dados/LinkedList.py
+++ Estrutura de Dados/LinkedList.py
@@ -1,32 +1,14 @@
-"""
-Singly  
-                   Elemento   Próximo
-HEAD[4800] -> Node   [10]     [4900]  -> Node [20][5000] -> Node [7][null]
-
-
-Doubly
-
-HEAD -> Node <-> Node <-> Node
-   null <-
-
-
-Operação                                    Singly  Doubly
-Acesso a um elemento                        O(n)    O(n)
-Adicionar/remover posição do iterador       O(1)    O(1)
-Adicionar/remover primeiro elemento         O(1)    O(1)
-Adicionar último elemento                   O(1)    O(1)
-Remover último elemento                     O(n)    O(1)
-"""
-
 class Node:
+
     def __init__(self, data):
-        self.data = data
-        self.next = None 
+        self.data = data 
+        self.next = None
 
 class LinkedList:
+
     def __init__(self):
         self.head = None
-
+    
     def append(self, data):
 
         node = Node(data)
@@ -42,100 +24,118 @@ class LinkedList:
 
         current_node.next = node
         return
-
+    
     def length(self):
-        if self.head == Node:
+
+        if self.head == None:
             return 0
 
+        total = 0
         current_node = self.head
 
-        total = 0
-
-        while current_node:
+        while current_node != None:
             current_node = current_node.next
             total += 1
 
         return total
 
     def to_list(self):
-        node_data = []
-        current_node = self.head
-
-        while current_node:
-            node_data.append(current_node.data)
-            current_node = current_node.next
-
-        return node_data
     
-    def display(self):
-
-        node = self.head
-
-        if node is None:
-            print("Linked List empty.")
-        
-        while node:
-           print(node.data)
-           node = node.next
-        print("-" * 8)
-    
-    def get(self, index):
-
-        if index >= self.length() or index < 0:
-            print("Error: Index out of range")
-            return None
-
-        current_idx = 0
+        lista = []
         current_node = self.head
 
         while current_node != None:
+            lista.append(current_node.data)
+            current_node = current_node.next
+            
+        
+        return lista
+    
+    def display(self):
+
+        current_node = self.head
+
+        if current_node is None:
+            print("Error: Empty Linked List.")
+
+        while current_node != None:
+            print(current_node.data)
+            current_node = current_node.next
+
+    def get_value(self, index):
+
+        if index > self.length() or index < 0:
+            print("Error: Index out of range.")
+
+        current_node = self.head
+        current_idx = 0
+
+        while current_node != None:
             if current_idx == index:
-                return current_node.data
+                    return current_node.data
+            
             current_node = current_node.next
             current_idx += 1
+    
+    def get_index(self, data):
+
+        if data == self.head.data:
+            return 0
+
+        current_node = self.head
+        current_idx = 0
+
+        while current_node != None:
+            if data == current_node.data:
+                return current_idx
+            
+            current_node = current_node.next
+            current_idx += 1
+        
+        else:
+            print("Error: Item not found.")
 
     def reverse(self):
-        
+
         previous_node = None
         current_node = self.head
 
         while current_node != None:
-            current_node.next, previous_node, current_node = previous_node, current_node, current_node.next
+            previous_node, current_node, current_node.next = current_node, current_node.next, previous_node
+        
         self.head = previous_node
     
     def search(self, data):
-        
-        if self.head == None:
-            print("Error: Empty list")
-            return None
 
+        if self.length() == 0:
+            print("Error: Empty Linked List.")
+            return None
+        
         current_node = self.head
 
         while current_node != None:
             if current_node.data == data:
                 print("Item found.")
                 return 
-            current_node = current_node.next
         else:
-            print("Item not found.")
-        
-    
-    def remove_first_item(self):
-        
-        if self.head == None:
-            print("Error: Empty list")
-            return None
+            print("Error: Item not found.")
 
-        self.head = self.head.next
-    
+    def remove_first_item(self):
+
+        if self.head == None:
+            print("Error: Empty Linked List.")
+
+        current_node = self.head
+        self.head = current_node.next
+
     def remove_last_item(self):
 
         self.reverse()
         self.remove_first_item()
         self.reverse()
-    
+
     def add_first_item(self, data):
-        
+
         node = Node(data)
 
         node.next = self.head
@@ -150,13 +150,13 @@ class LinkedList:
     def remove_by_value(self, data):
 
         if self.head == None:
-            print("Error: Empty list")
+            print("Error: Empty Linked List.")
             return None
 
-        if data == self.head.data:
+        if self.data == data:
             self.remove_first_item()
             return
-        
+
         current_node = self.head
 
         while current_node != None:
@@ -165,13 +165,13 @@ class LinkedList:
 
             if current_node.data == data:
                 previous_node.next = current_node.next
-                return
+                return 
         else:
-            print("Item not found.")
-         
-    def insert_by_index(self, index, data):
+            print("Error: Item not found.")
 
-        if index >= self.length() or index < 0:
+    def insert_by_index(self, index, data):
+        
+        if index > self.length() or index < 0:
             print("Error: Index out of range")
             return None
         
@@ -193,3 +193,28 @@ class LinkedList:
                 return
 
             current_idx += 1
+
+    def remove_by_index(self, index):
+
+        if index > self.length() or index < 0:
+            print("Error: Index out of range")
+        
+        if index == 0:
+            self.remove_first_item()
+            return
+        
+        current_node = self.head
+        current_idx = 0
+
+        while current_node != None:
+            previous_node = current_node
+            current_node = current_node.next
+
+            if current_idx == index:
+                previous_node.next = current_node.next
+                return
+
+            current_idx += 1
+
+        else:
+            print("Error: Item not found.")
