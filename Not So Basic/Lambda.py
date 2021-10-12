e097694339defc6bdfd1755181fd864d981f904a
Victor Balbino <victorbalbinoaraujo1@outlook.com>
2021-10-12 18:19:20 -0300

    Minor adjustments

--- Not So Basic/Lambda.py
+++ Not So Basic/Lambda.py
@@ -1,4 +1,6 @@
 def square_def(num): return num ** 2
+
+
 # Atribuir uma função simples a uma variável
 
 square_lambda = lambda num: num ** 2
@@ -7,8 +9,8 @@ print(square_def(3))
 print(square_lambda(3))
 
 even = lambda num: num % 2 == 0
-test = [even(x) for x in range(5)] # 0 1 2 3 4
-print(test)                        # T F T F T
+test = [even(x) for x in range(5)]  # 0 1 2 3 4
+print(test)  # T F T F T
 
 first_char = lambda c: c[0]
 print(first_char("Lambda"))
@@ -31,18 +33,20 @@ print(divide(9, 2))
 
 media_ponderada = lambda x, y: ((x * 4) + (y * 6)) / 10
 
+
 def conceito(medias):
     conceitos = []
     for i in medias:
         if i < 4.9:
             conceitos.append("D")
-        elif i >= 5 and i <= 6.9:
+        elif 5 <= i <= 6.9:
             conceitos.append("C")
-        elif i >= 7.0 and i <= 8.9:
+        elif 7.0 <= i <= 8.9:
             conceitos.append("B")
-        elif i >= 9.0 and i <= 10.0:
+        elif 9.0 <= i <= 10.0:
             conceitos.append("A")
     return conceitos
+
 
 turma = []
 for i in range(10):
@@ -55,17 +59,18 @@ conceito(turma)
 
 prestacao = lambda financ, prazo, taxa: financ * (((1 + taxa) ** prazo) * taxa) / (((1 + taxa) ** prazo) - 1)
 
+
 def percentual(prazo):
     if prazo == 6:
-        perc = 0.07/12
+        perc = 0.07 / 12
     elif prazo == 12:
-        perc = 0.10/12
+        perc = 0.10 / 12
     elif prazo == 18:
-        perc = 0.12/12
+        perc = 0.12 / 12
     elif prazo == 24:
-        perc = 0.15/12
+        perc = 0.15 / 12
     elif prazo == 36:
-        perc = 0.18/12
+        perc = 0.18 / 12
     return perc
 
 
@@ -77,22 +82,20 @@ while valor_financiamento != -1:
     print(f"Prestação do financiamento: {p}")
     valor_financiamento = float(input("Valor do financiamento: "))
 
-
 maior = lambda a, b: a if a > b else b
 test1 = maior(12, 7)
 test2 = maior(7, 12)
 
 dezoito = lambda a: print("Maior de idade") if a >= 18 else print("Menor de idade")
-test3 = dezoito(5)
-test4 = dezoito(22)
+dezoito(5)
+dezoito(22)
 
-c_to_f = lambda x: (x * (9/5)) + 32
-f_to_c = lambda x: (x - 32) * 5/9
+c_to_f = lambda x: (x * (9 / 5)) + 32
+f_to_c = lambda x: (x - 32) * 5 / 9
 k_to_c = lambda x: x - 273.15
 c_to_k = lambda x: x + 273.15
-f_to_k = lambda x: ((x - 32) * 5/9) + 273.15
-k_to_f = lambda x: (x - 273.15) * 9/5 + 32
-
+f_to_k = lambda x: ((x - 32) * 5 / 9) + 273.15
+k_to_f = lambda x: (x - 273.15) * 9 / 5 + 32
 
 import random
 
@@ -103,8 +106,7 @@ print(numeros)
 pares = filter(lambda x: x % 2 == 0, numeros)
 print(list(pares))
 
-
-import random 
+import random
 
 mpf = lambda: print("Minha primeira função!")
 mpf()
--- Not So Basic/List Comprehension.py
+++ Not So Basic/List Comprehension.py
@@ -4,21 +4,18 @@ square_list = [x * x for x in range(10)]
 even_squares = [x * x for x in even_numbers]
 odd_squares = [x * x for x in odd_numbers]
 years = [x for x in range(1900, 2010, 10)]
-squares_dict = {x : x * x for x in range(11)}
+squares_dict = {x: x * x for x in range(11)}
 squares_set = {x * x for x in [3, 7, 5, 9, 2]}
 pairs = [(x, y)
-        for x in range(4)
-        for y in range(4)]
+         for x in range(4)
+         for y in range(4)]
 increasing_pairs = [(x, y)
-                   for x in range(4)
-                   for y in range(x+1, 4)]
-
-
+                    for x in range(4)
+                    for y in range(x + 1, 4)]
 
 list1 = [int(input()) for x in range(5)]
 for i, item in enumerate(list1):
     print(f"{i}: {item}")
-
 
 list2 = [float(input()) for x in range(10)]
 list2.reverse()
@@ -43,9 +40,9 @@ odd = [x for x in list5 if x % 2 == 1]
 print(even)
 print(odd)
 
-
 avg_temp = [float(input()) for x in range(12)]
-month = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+month = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro',
+         'Novembro', 'Dezembro']
 annual = 0
 for x in avg_temp:
     annual += x
@@ -55,13 +52,14 @@ for i, item in enumerate(avg_temp):
     if item > annual:
         print(f"{i + 1} - {month[i]}")
 
-
 list_l = [float(input()) for x in range(10)]
 list_l2 = [x * 2 for x in list_l]
 print(list_l2)
 
-questions = ["Telefonou para a vítima?", "Esteve no local do crime?", "Mora perto da vítima?", "Tinha dívidas com a vítima?", "Já trabalhou com a vítima?"]
-positives = ['S', 'SIM', 'POSITIVO', 'SI', 'YES', 'YEAH', 'AHAM', 'É', 'AFIRMATIVO', 'CERTO', 'ISSO', 'COM CERTEZA', 'DE ACORDO', 'EFETIVAMENTE', 'CLARO', 'JÁ', 'TINHA', 'MORO', 'ESTIVE', 'TELEFONEI']
+questions = ["Telefonou para a vítima?", "Esteve no local do crime?", "Mora perto da vítima?",
+             "Tinha dívidas com a vítima?", "Já trabalhou com a vítima?"]
+positives = ['S', 'SIM', 'POSITIVO', 'SI', 'YES', 'YEAH', 'AHAM', 'É', 'AFIRMATIVO', 'CERTO', 'ISSO', 'COM CERTEZA',
+             'DE ACORDO', 'EFETIVAMENTE', 'CLARO', 'JÁ', 'TINHA', 'MORO', 'ESTIVE', 'TELEFONEI']
 score = 0
 
 for x in questions:
