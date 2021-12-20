0115afa086d62a3a96ccffdf40320d6a13bc381f
victorbalbinoaraujo <victorbalbinoaraujo1@outlook.com>
2021-12-20 10:24:46 -0300

    Flask Tutorial

--- flask/tutorial/app.py
+++ flask/tutorial/app.py
@@ -1,55 +1,59 @@
-# Setup
-#   export FLASK_APP=app.py
-#   flask run
+# To RUN -> Terminal
+# export FLASK_APP=app.py
+# flask run
 
-from flask import Flask, jsonify, request
-
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-# Simple 'Hello, World!'
-@app.route('/', methods=['GET'])
+
+@app.route('/')
 def hello_world():
-    return "Hello, World!"
+    return 'Hello World!'
 
-# Simple math
-@app.route('/simple-math', methods=['GET'])
+
+@app.route('/hithere')
+def hithere_everyone():
+    return 'I just hit hi there (or hit here?)'
+
+
+@app.route('/simple_math')
 def simple_math():
-    return str(1 + 1)
+    a = 34 + 6
+    a_str = str(a)
+    return a_str
 
-# Returning JSONs
-@app.route('/json', methods=['GET'])
+
+@app.route('/json_example')
 def json_example():
-    json_ex = {
-        'Name'   : 'John Doe',
-        'Age'    : 30,
-        'Phones' : [
+    json = {
+        'field_1': 'abc',
+        'field_2': 'def'
+    }
+    return jsonify(json)
+
+
+@app.route('/person')
+def person():
+    person_json = {
+        'Name': 'Victor',
+        'Age': 21,
+        'Emails': [
+                'fakemail@fake.com',
+                'emailfake@email.com'
+        ],
+        'Phones': [
             {
-                'Phone name'   : 'Samsung',
-                'Phone number' : 12346578
+                'phone_name': 'Iphone8',
+                'phone_number': 1234567890
             },
             {
-                'Phone name'   : 'Motorola',
-                'Phone number' : 87654321
+                'phone_name': 'Samsung Galaxy',
+                'phone_number': 1234098765
             }
         ]
     }
+    return jsonify(person_json)
 
-    return jsonify(json_ex)
-
-# POST Method
-@app.route('/sum-two-numbers', methods=['POST'])
-def sum_two_numbers():
-    # Get x, y from posted data
-    data_numbers = request.get_json()
-
-    # Handle error
-    if "x" not in data_numbers:
-        return "ERROR", 305
-
-    # Sum the two numbers
-    z = data_numbers["x"] + data_numbers["y"]
-
-    return jsonify({"Sum" : z}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
