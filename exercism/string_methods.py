b959bd5a9039751e8d973ba0190840aba2aff0e6
victorbalbinoaraujo <victorbalbinoaraujo1@outlook.com>
2021-12-21 20:40:38 -0300

    Exercism - String Methods

---
+++ exercism/string_methods.py
@@ -0,0 +1,14 @@
+def capitalize_title(title):
+    return title.title()
+
+
+def check_sentence_ending(sentence):
+    return sentence.endswith('.')
+
+
+def clean_up_spacing(sentence):
+    return sentence.strip(' ')
+
+
+def replace_word_choice(sentence, old_word, new_word):
+    return sentence.replace(old_word, new_word)
